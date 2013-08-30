08c85c69c58d6605cc738fb9156a603f1d9926fc
Christopher Stevenson <ccstevenson@live.com>
2013-08-30 10:30:33 -0600

    Updated comment formatting.

--- Eval/evalTest.py
+++ Eval/evalTest.py
@@ -19,7 +19,7 @@ print 'Sum of all salaries:', salarysum
 # eval(expression[, allowedGlobals[, allowedLocals]])
 salarysum = eval('sum(rate * hpy for rate, hpy in employees)', 
                  {'__builtins__':None, 'sum':sum}, # Globals--only sum() allowed.
-                 {'employees':employees,}) #Locals --only employee allowed.
+                 {'employees':employees,}) # Locals --only employee allowed.
 
 print 'Sum of all salaries:', salarysum
 # Output: Sum of all salaries: 179852.5
@@ -27,10 +27,11 @@ print 'Sum of all salaries:', salarysum
 # Without allowing sum
 '''salarysum = eval('sum(rate * hpy for rate, hpy in employees)', 
                  {'__builtins__':None,}, # Globals--we've disallowed sum().
-                 {'employees':employees,}) #Locals --only employee allowed.
+                 {'employees':employees,}) # Locals --only employee allowed.
 
 print 'Sum of all salaries:', salarysum'''
 # Fails! 'NameError: name 'sum' is not defined'
+
 
 
 
